da5e2a87382c569a574bbc7b5384f6405f83cff5
ulfw <ulf.worsoe@mosek.com>
2022-02-08 20:09:49 +0000

    debugging solver server use

--- src/MosekTools.jl
+++ src/MosekTools.jl
@@ -81,7 +81,6 @@ mutable struct Optimizer  <: MOI.AbstractOptimizer
     dpars :: Dict{String, Float64}
     # String parameters, i.e. parameters starting with `MSK_SPAR_`
     spars :: Dict{String, AbstractString}
-
     has_variable_names::Bool
     constrnames :: Dict{String, Vector{MOI.ConstraintIndex}}
     # Mosek only support names for `MOI.ScalarAffineFunction` so we need a
@@ -307,6 +306,7 @@ function matrix_solution(m::Optimizer, sol)
 end
 
 function MOI.optimize!(m::Optimizer)
+    println("------ fallback = $(m.fallback)")
     m.trm = if m.fallback == nothing; optimize(m.task) else optimize(m.task,m.fallback) end
     m.solutions = MosekSolution[]
     # If the problem is conic but a starting value is set,
